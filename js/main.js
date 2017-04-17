5c56a05be0f2f4700937ddede8dd799f2f073904
PhongNT <tienphongse@gmail.com>
2017-04-17 15:05:40 +0700

    starting coding

---
+++ js/main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$("#item-1").hover(function(){
+	    $("#item-1-hidden").show();
+	    }, function(){
+	    $("#item-1-hidden").hide();
+	});
+	$("#item-2").hover(function(){
+	    $("#item-2-hidden").show();
+	    }, function(){
+	    $("#item-2-hidden").hide();
+	});
+	$("#item-3").hover(function(){
+	    $("#item-3-hidden").show();
+	    }, function(){
+	    $("#item-3-hidden").hide();
+	});
+});
